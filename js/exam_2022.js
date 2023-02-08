89553f7ebcea463a55b7a1949923021f421d9f3e
simamori <210088ik@yse-c.net>
2023-02-08 12:03:20 +0900

    カラー変更機能追加

--- js/exam_2022.js
+++ js/exam_2022.js
@@ -1,3 +1,16 @@
+function changeColor(newColor) {
+    var bodyElement = document.getElementById('page_color');
+    if (newColor == 'black') {
+        bodyElement.style.color = 'black';
+        bodyElement.style.backgroundColor = 'white'
+    }
+    else {
+        bodyElement.style.color = 'white';
+        bodyElement.style.backgroundColor = 'black'
+    }
+}
+
 function clickAlert() {
     alert('Hello World!');
 }
+
